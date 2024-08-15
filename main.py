0d926f1e70baa7d338fc6a018367da236c8859a0
aminakm123 <shamrifari@gmail.com>
2024-08-15 09:24:22 +0530

    HTTPException

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException # type: ignore
 
 app = FastAPI()
 
@@ -11,16 +11,22 @@ def get_top_news(count: int = 10):
     # print("response.json() ",response.json())
     # print("response.json()[:count] ",response.json()[:count])
     if response.status_code == 200:
-        top_stories_ids = response.json()[:count]
-        top_stories = []
+        try:
+            top_stories_ids = response.json()[:count]
+            top_stories = []
 
-        for top_story_id in top_stories_ids:
-            top_story_url = f"https://hacker-news.firebaseio.com/v0/item/{top_story_id}.json"
-            response = requests.get(top_story_url)
-            # print("response status code ",response.status_code)
-            # print("top story - response.json() ",response.json())
-            top_story = response.json()
-            top_stories.append(top_story)
-        return {"top news":top_stories}
+            for top_story_id in top_stories_ids:
+                top_story_url = f"https://hacker-news.firebaseio.com/v0/item/{top_story_id}.json"
+                response = requests.get(top_story_url)
+                if response.status_code == 200:
+                    # print("response status code ",response.status_code)
+                    # print("top story - response.json() ",response.json())
+                    top_story = response.json()
+                    top_stories.append(top_story)
+                else:
+                    raise HTTPException(status_code=500, detail="Failed to retrieve the stop story")
+            return {"top news":top_stories}
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
     else:
-        return {"error": "Failed to retrieve top stories"}
+        raise HTTPException(status_code=500, detail="Failed to retrieve top stories")
